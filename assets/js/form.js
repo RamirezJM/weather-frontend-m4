6e69b21386b4fded39b023da521a37785d84fe0c
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-01-04 23:41:20 -0300

    feat: agregar script validación de formulario

---
+++ assets/js/form.js
@@ -0,0 +1,16 @@
+
+const form = document.querySelector('.needs-validation');
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+
+  if (!form.checkValidity()) {
+    ev.stopPropagation();
+    form.classList.add('was-validated');
+    return
+  } else {
+    alert('Formulario enviado. Pronto nos comunicaremos contigo.');
+    form.classList.remove('was-validated');
+    form.reset();
+  }
+});
